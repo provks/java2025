e9818e40c5ecc69e9e5768979cb175f435d498e2
varun <coldvks@gmail.com>
2025-01-18 21:26:22 +0530

    1.IntroToJava

---
+++ HelloWorld.java
@@ -0,0 +1,25 @@
+package Java;
+class HelloWorld {
+	// what is comment?
+	public static void main(String args[]) {
+		System.out.println("hello world");	// printing hello world!
+		// System.out.println("hello Coding Ninjas!");
+		System.out.println("My name is 'Varun'!");
+		// System.out.print("I live in Delhi.");
+		/*
+		 * This is the multiline comment
+I am just learning Java
+		 * and having fun while learning it.
+		 */
+		System.out.println(1+2);
+		// int a = 1;	// declaration + initialize
+		// int b = 2;
+		int a = 10, e, b = 20, d;	// d is declared
+		int c = a + b;
+		d = 40;	// d is initialized
+		// System.out.println(e);
+		System.out.println(c);	//30
+		System.out.println(c+d);	//70
+
+	}
+}
