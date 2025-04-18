014017d86d57f12a01899275e4cb4a69acb0f0b4
varun <coldvks@gmail.com>
2025-04-18 23:06:59 +0530

    learning getter and setter, constructor, this, final, static

--- Oops/Student.java
+++ Oops/Student.java
@@ -7,19 +7,66 @@ public class Student {
     public int rollNo;
     public String name;
     private String address;
+    double cgpa;
+    // final double conversionFactor = 0.95;
+    final static double conversionFactor = 0.95;
+    static int totalStudents;
+
+    // constructor
+    Student() {
+        // this("student");
+        // conversionFactor = 0.95;
+        totalStudents++;
+    }
+
+    // public Student(String name) {
+    //     this.name = name;
+    // }
+    
+    // public Student(String studentName, int rollNum, String studentAddress) {
+    //     name = studentName;
+    //     rollNo = rollNum;
+    //     address = studentAddress;
+    // }
 
     //methods
     void print() {
-        System.out.println("My name is " + name + " and roll no. is " + rollNo);
+        System.out.println("My name is " + name + " and roll no. is " + rollNo + ". I live in " + address + " factor " + conversionFactor);
+    }
+
+    // Getter (to give read access)
+    public String getAddress() {
+        return address;
+    }
+
+    // Setter (to write access)
+    public void setAddress(String address) {
+        if (address.length() < 5) {
+            System.out.println("Invalid address, it should be of atleast 5 characters");
+            return;
+        }
+        // System.out.println(address);
+        System.out.println("this" +  this);
+        this.address = address;
     }
 
     public static void main(String[] args) {
         // Scanner sc = new Scanner(System.in);
-        Student s1 = new Student();
-        System.out.println(s1);
+        // Student s1 = new Student("varun");
+        // Student s2 = new Student("varun", 51, "New Delhi");
+        // Student s3 = new Student("varun", 51);
+        Student s4 = new Student();
+        System.out.println(s4.conversionFactor);
+        // System.out.println(s1);
+        // s4.conversionFactor = 0.9;
 
-        System.out.println(s1.name);
-        System.out.println(s1.rollNo);
-        System.out.println(s1.address);
+        // System.out.println(s4.name);
+        // s1.print();
+        // s2.print();
+        s4.print();
+        // System.out.println(s4.totalStudents);
+        System.out.println(Student.totalStudents);
+        // System.out.println(s1.rollNo);
+        // System.out.println(s1.address);
     }
 }
--- Oops/StudentUse.java
+++ Oops/StudentUse.java
@@ -12,28 +12,40 @@ public class StudentUse {
         System.out.println(s2);
 
         // how to get the value
-        System.out.println("s1 name: " + s1.name);
-        System.out.println("s1 rollNo: " + s1.rollNo);
-        System.out.println("s1 rollNo: " + s1.address);
+        // System.out.println("s1 name: " + s1.name);
+        // System.out.println("s1 rollNo: " + s1.rollNo);
+        // System.out.println("s1 rollNo: " + s1.address); // address is private property
 
         // update the value of properties/attribute
         s1.name = "piyush";
         s1.rollNo = 33;
 
-        System.out.println("s1 name: " + s1.name);
-        System.out.println("s1 rollNo: " + s1.rollNo);
+        // System.out.println("s1 name: " + s1.name);
+        // System.out.println("s1 rollNo: " + s1.rollNo);
 
         s2.name = "Arbaz";
         s2.rollNo = 1;
         
-        System.out.println("s2 name: " + s2.name);
-        System.out.println("s2 rollNo: " + s2.rollNo);
+        // System.out.println("s2 name: " + s2.name);
+        // System.out.println("s2 rollNo: " + s2.rollNo);
 
         // calling class method
         s1.print();
         s2.print();
 
+        // Getter and Setter
+        // accessing private address with getter
+        // System.out.println("Address S1: " + s1.getAddress());
+        
+        // setter
+        // s1.setAddress("Delhi");
+        // System.out.println("Address S1: " + s1.getAddress());
+        
+        // s2.setAddress("Mumbai");
+        // System.out.println("Address S2: " + s2.getAddress());
 
+        // accessing final property
+        System.out.println(s1.conversionFactor);
 
     }
 }
