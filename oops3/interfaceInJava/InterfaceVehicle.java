0c5fa9bd2fd704d82763fa7c4e1d547b10c7e6cb
varun <coldvks@gmail.com>
2025-05-01 21:11:11 +0530

    final commit for OOPS3_part2, omit public abstract

--- oops3/interfaceInJava/InterfaceVehicle.java
+++ oops3/interfaceInJava/InterfaceVehicle.java
@@ -1,6 +1,8 @@
 package Java.oops3.interfaceInJava;
 
 public interface InterfaceVehicle {
-    public abstract boolean isMotorized();
-    public abstract String getCompany();
+    // public abstract boolean isMotorized();
+    // public abstract String getCompany();
+    boolean isMotorized();
+    String getCompany();
 }
