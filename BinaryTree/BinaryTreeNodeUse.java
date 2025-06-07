eb26386ab3a0be8a6d7be65a35840113bec73b95
varun <coldvks@gmail.com>
2025-06-07 23:08:51 +0530

    take input for binary tree + count number of nodes in binary tree

--- BinaryTree/BinaryTreeNodeUse.java
+++ BinaryTree/BinaryTreeNodeUse.java
@@ -1,5 +1,7 @@
 package Java.BinaryTree;
 
+import java.util.Scanner;
+
 public class BinaryTreeNodeUse {
 
     public static void printBinaryTreeDetailed(BinaryTreeNode<Integer> root) {
@@ -41,27 +43,112 @@ public class BinaryTreeNodeUse {
     //     // }
     // }
 
-    public static void main(String[] args) {
-        // first node, root
-        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(1);
-        // children of root
-        BinaryTreeNode<Integer> rootLeft = new BinaryTreeNode<>(2);
-        BinaryTreeNode<Integer> rootRight = new BinaryTreeNode<>(3);
+    // taking input
+    // public static BinaryTreeNode<Integer> takeInput() {
+    //     // step 1: taking input from user
+    //     System.out.print("Enter root data");
+    //     Scanner sc = new Scanner(System.in);
+    //     int rootData = sc.nextInt();
+
+    //     // base case
+    //     if (rootData == -1) {
+    //         return null;
+    //     }
+
+    //     // step2: create binary tree node from rootData
+    //     BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+
+    //     // call recursion
+    //     // left subtree
+    //      BinaryTreeNode<Integer> rootLeft = takeInput();
+    //     // right subtree
+    //      BinaryTreeNode<Integer> rootRight = takeInput();
+
+    //     // make connections
+    //     root.left = rootLeft;
+    //     root.right = rootRight;
+
+    //     return root;
+    // }
+
+    public static BinaryTreeNode<Integer> takeInputBetter(boolean isRoot, int parentData, boolean isLeft) {
+        // step 1: taking input from user
+        if (isRoot) {
+            System.out.print("Enter root data: ");
+        } else {
+            // left of root
+            if (isLeft) {
+                System.out.println("Enter left child of " + parentData + ": ");
+            } else {
+                // right of root
+                System.out.println("Enter right child of " + parentData + ": ");
+
+            }
+        }
+        Scanner sc = new Scanner(System.in);
+        int rootData = sc.nextInt();
+
+        // base case
+        if (rootData == -1) {
+            return null;
+        }
+
+        // step2: create binary tree node from rootData
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+
+        // call recursion
+        // left subtree
+            BinaryTreeNode<Integer> rootLeft = takeInputBetter(false, rootData, true);
+        // right subtree
+            BinaryTreeNode<Integer> rootRight = takeInputBetter(false, rootData, false);
 
         // make connections
         root.left = rootLeft;
         root.right = rootRight;
 
-        // printBinaryTree(root);
+        return root;
+    }
 
-        // children of rootLeft
-        BinaryTreeNode<Integer> four = new BinaryTreeNode<>(4);
-        rootLeft.right = four;
-        // children of rootRight
-        BinaryTreeNode<Integer> five = new BinaryTreeNode<>(5);
-        rootRight.left = five;
+    // count number of nodes in a binary tree
+    public static int countNumberOfNodes(BinaryTreeNode<Integer> root) {
+        // base case: root is null
+        if (root == null) {
+            return 0;
+        }
+
+        // recursive call
+        int leftSubtreeNodeCount = countNumberOfNodes(root.left);
+        int rightSubtreeNodeCount = countNumberOfNodes(root.right);
+
+        // small calc
+        return leftSubtreeNodeCount + rightSubtreeNodeCount + 1;
+    }
+
+    public static void main(String[] args) {
+        // first node, root
+        // BinaryTreeNode<Integer> root = new BinaryTreeNode<>(1);
+        // // children of root
+        // BinaryTreeNode<Integer> rootLeft = new BinaryTreeNode<>(2);
+        // BinaryTreeNode<Integer> rootRight = new BinaryTreeNode<>(3);
+
+        // // make connections
+        // root.left = rootLeft;
+        // root.right = rootRight;
+
+        // // printBinaryTree(root);
+
+        // // children of rootLeft
+        // BinaryTreeNode<Integer> four = new BinaryTreeNode<>(4);
+        // rootLeft.right = four;
+        // // children of rootRight
+        // BinaryTreeNode<Integer> five = new BinaryTreeNode<>(5);
+        // rootRight.left = five;
+
+        BinaryTreeNode<Integer> root = takeInputBetter(true, 0, true);
 
         printBinaryTreeDetailed(root);
+        int numOfNodes = countNumberOfNodes(root);
+        System.out.println("Number of Nodes: " + numOfNodes);
 
     }
     
